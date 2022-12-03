b19f0e09f9cca0c0664fc6e19c95f775e1566cfa
ilia <iliaman1993@gmail.com>
2022-12-03 10:11:55 +0300

    complete game with simple animation.py

--- small cours/simple animation.py
+++ small cours/simple animation.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 
 class MyAnimation(tk.Tk):
@@ -7,27 +8,31 @@ class MyAnimation(tk.Tk):
         self.title('simple game')
         self.geometry('300x200')
         self.iconphoto(False, tk.PhotoImage(file='img/animation.png'))
+        self.config(cursor='man')
 
         self.canva = tk.Canvas(width=300, height=200, bg="white")
-        self.ball = self.canva.create_oval(0, 100, 40, 140, fill='green')
+        self.scaled_dog = Image.open('img/happy_dog.png').resize((40, 40))
+        self.dog = ImageTk.PhotoImage(self.scaled_dog)
+        self.dog_tk = self.canva.create_image(10, 10, anchor='nw', image=self.dog)
 
         self.canva.pack()
 
         self.canva.bind('<Button-1>', self.motion)
 
     def motion(self, event):
-        while event.x != self.canva.coords(self.ball)[2] or event.y != self.canva.coords(self.ball)[3]:
-            if self.canva.coords(self.ball)[2] < event.x:
-                self.canva.move(self.ball, 1, 0)
-            if self.canva.coords(self.ball)[3] < event.y:
-                self.canva.move(self.ball, 0, 1)
-            if self.canva.coords(self.ball)[2] > event.x:
-                self.canva.move(self.ball, -1, 0)
-            if self.canva.coords(self.ball)[3] > event.y:
-                self.canva.move(self.ball, 0, -1)
+        if self.canva.coords(self.dog_tk)[0] < event.x:
+            self.canva.move(self.dog_tk, 1, 0)
+        if self.canva.coords(self.dog_tk)[1] < event.y:
+            self.canva.move(self.dog_tk, 0, 1)
+        if self.canva.coords(self.dog_tk)[0] > event.x:
+            self.canva.move(self.dog_tk, -1, 0)
+        if self.canva.coords(self.dog_tk)[1] > event.y:
+            self.canva.move(self.dog_tk, 0, -1)
+        if event.x != self.canva.coords(self.dog_tk)[0] or event.y != self.canva.coords(self.dog_tk)[1]:
+            self.after(10, self.motion, event)
 
-        # if self.canva.coords(self.ball)[2]<300:
-        #     self.after(10, self.motion(event))
+    def show_current_position(self):
+        print(self.canva.coords(self.dog_tk))
 
 
 if __name__ == '__main__':
